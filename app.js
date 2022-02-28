d9e846af68099ad969796821106a8c2e8bfdbd95
Bondhan Mondal <bondhan2725@gmail.com>
2022-03-01 00:46:29 +0600

    28-2 shopping cart handle decrease and update price

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
+function updateCaseNumber(isIncreasing){
+    const csaeInput = document.getElementById('case-number');
+    let caseNumber = csaeInput.value;
+    if(isIncreasing == true){
+        caseNumber = parseInt(caseNumber) + 1;
+    }
+    else if(caseNumber > 0){
+        caseNumber = parseInt(caseNumber) - 1;
+    }
+    csaeInput.value = caseNumber;
+    //update case total
+    const caseTotal = document.getElementById('case-total');
+    caseTotal.innerText = caseNumber * 59;
+}
+
 //handle case plus event
 document.getElementById('case-plus').addEventListener('click', function(){
-    const csaeInput = document.getElementById('case-number');
-    const caseNumber = csaeInput.value;
-    csaeInput.value = parseInt(caseNumber) + 1;
-})
+    updateCaseNumber(true);
+});
+//handle case minus event
+document.getElementById('case-minus').addEventListener('click', function(){
+    updateCaseNumber(false);
+});

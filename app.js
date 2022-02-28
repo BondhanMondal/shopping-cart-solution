4feb61e68cfe5fdc1ab31a0bbf045246e77d16ee
Bondhan Mondal <bondhan2725@gmail.com>
2022-03-01 00:28:25 +0600

    28-1 Module Overview and shopping cart increase case count

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 //handle case plus event
 document.getElementById('case-plus').addEventListener('click', function(){
+    const csaeInput = document.getElementById('case-number');
+    const caseNumber = csaeInput.value;
+    csaeInput.value = parseInt(caseNumber) + 1;
 })
